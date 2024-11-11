69da89a7ffe01e693b6bc19ba8e5c7eba9aa5494
Leon-CYL <leonlin0106@gmail.com>
2024-11-10 21:12:25 -0800

    Fix User API and work correctly now

---
+++ backend/src/main/java/com/spring/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.spring.controller;
+
+import com.spring.model.User;
+import com.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> getUserProfile(
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        return new ResponseEntity<>(userService.findUserProfileByJwt(jwt), HttpStatus.OK);
+    }
+}
