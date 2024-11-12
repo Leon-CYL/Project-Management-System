4dca8ce12ba152d37afa1441f6eec166c57adabf
Leon-CYL <leonlin0106@gmail.com>
2024-11-11 21:31:49 -0800

    Finish Filter section for the Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import { Button } from './components/ui/button'
+import Home from './pages/Home/home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button>click</Button>
+    <Home/>
     </>
   )
 }
