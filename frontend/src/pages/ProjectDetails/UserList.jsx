b4bc7f539930939ee9a4fd3de511636e1a1d3c67
Leon-CYL <leonlin0106@gmail.com>
2024-11-13 22:28:34 -0800

    Finish Project detail page

--- frontend/src/pages/ProjectDetails/UserList.jsx
+++ frontend/src/pages/ProjectDetails/UserList.jsx
@@ -7,14 +7,20 @@ const UserList = () => {
         <div className="rounded-md border">
           <p className="py-2 px-3">{"Leon" || "unassignee"}</p>
         </div>
-        <div className="py-2 group hover:bg-slate-800 flex cursor-pointer items-center space-x-4 rounded-md border px-4">
-          <Avatar>
-            <AvatarFallback>L</AvatarFallback>
-          </Avatar>
-          <div className="space-y-1">
-            <p className="text-sm leading-none">@Leon</p>
+        {[1, 1, 1, 1].map((item) => (
+          <div
+            key={item}
+            className="py-2 group hover:bg-slate-800 flex cursor-pointer items-center space-x-4 rounded-md border px-4"
+          >
+            <Avatar>
+              <AvatarFallback>L</AvatarFallback>
+            </Avatar>
+            <div className="space-y-1">
+              <p className="text-sm leading-none">Leon</p>
+              <p className="text-sm text-muted-foreground">@Leon</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
