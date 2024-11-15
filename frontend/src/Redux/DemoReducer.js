68dbc64b4ba106f0c206170e7767e8cfec44d8be
Leon-CYL <leonlin0106@gmail.com>
2024-11-15 15:47:58 -0800

    Finish Auth & Project

---
+++ frontend/src/Redux/DemoReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+  jwt: null,
+  projectSize: 0,
+};
+
+const demoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      state;
+  }
+};
+export default demoReducer;
--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,1 +1,10 @@
-export const BASE_URL = "http://localhost:8181"
+import axios from "axios";
+
+export const API_BASE_URL = "http://localhost:8181";
+const api = axios.create({ baseURL: API_BASE_URL });
+const jwt = localStorage.getItem("jwt");
+
+api.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+api.defaults.headers.post["Content-Type"] = "application/json";
+
+export default api;
